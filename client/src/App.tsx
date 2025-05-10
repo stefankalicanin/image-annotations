b823a4c3c43bd0558cf1b1f9bde73870cbafe043
stefankalicanin13 <s.kalicanin@remotehands24-7.com>
2025-05-10 12:45:08 +0200

    Initial project setup - dockerize the app & install packages

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center w-full min-h-svh text-black text-xl font-bold">
+      Hello world
+    </div>
+  );
+}
+
+export default App;
