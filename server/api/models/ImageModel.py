7dd8c45dee7789752f1eac79095b361c1be78375
stefankalicanin13 <s.kalicanin@remotehands24-7.com>
2025-05-16 12:07:14 +0200

    fix:added python types for uuid;changed relationship between Image and Annotation to be 1-1

--- server/api/models/ImageModel.py
+++ server/api/models/ImageModel.py
@@ -1,13 +1,14 @@
 from typing import List
 from typing import Dict
 from typing import Any
-
+from uuid import UUID
 
 from sqlalchemy import String
-from sqlalchemy import UUID
+from sqlalchemy import UUID as sql_alchemy_UUUID
 from sqlalchemy import JSON
 from sqlalchemy import Integer
 from sqlalchemy import ForeignKey
+from sqlalchemy import UniqueConstraint
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
@@ -18,19 +19,17 @@ from api.database.database import Base
 class Image(Base):
     __tablename__ = "images"
 
-    id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True)
-    path: Mapped[str] = mapped_column(String(255), nullable=False)
-
-    annotations: Mapped[List["Annotation"]] = relationship(
-        back_populates="image", cascade="all, delete-orphan"
-    )
+    id: Mapped[UUID] = mapped_column(sql_alchemy_UUUID(as_uuid=True), primary_key=True)
+    path: Mapped[str] = mapped_column(String(255))
+    annotations: Mapped["Annotation"] = relationship(back_populates="image")
 
 
 class Annotation(Base):
     __tablename__ = "annotations"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    image_id: Mapped[UUID] = mapped_column(ForeignKey("images.id"), nullable=False)
-    data: Mapped[List[Dict[str, Any]]] = mapped_column(JSON, nullable=False)
-
+    image_id: Mapped[UUID] = mapped_column(ForeignKey("images.id"))
+    data: Mapped[List[Dict[str, Any]]] = mapped_column(JSON)
     image: Mapped["Image"] = relationship(back_populates="annotations")
+
+    __table_args__ = (UniqueConstraint("image_id"),)
